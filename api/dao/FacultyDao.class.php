8962c1ab4d56b707139caa928a1c62cb520288ec
VedranSelak <selakvedran@gmail.com>
2021-03-09 15:07:45 +0100

    basic dao setup

---
+++ api/dao/FacultyDao.class.php
@@ -0,0 +1,10 @@
+<?php
+  
+  class FacultyDao extends BaseDao {
+
+    public function add_faculty($faculty){
+
+    }
+  }
+
+ ?>
---
+++ api/dao/UserDao.class.php
@@ -0,0 +1,22 @@
+<?php
+class UserDao extends BaseDao {
+
+
+  public function get_user_by_email($email){
+
+  }
+
+  public function update(){
+
+  }
+
+  public function query(){
+
+  }
+
+  public function query_unique(){
+
+  }
+
+}
+ ?>
