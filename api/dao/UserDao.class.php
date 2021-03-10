5a691218b4e66cb2765ae4e51c351086096a6f80
VedranSelak <selakvedran@gmail.com>
2021-03-11 00:57:05 +0100

    added insert user

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -11,5 +11,17 @@ class UserDao extends BaseDao {
     return $this->query_unique("SELECT * FROM user WHERE id = :id",["id" => $id]);
   }
 
+  public function add_user($user){
+    $query = "INSERT INTO user (name, email, password, pins, date_of_joining, faculty_id, department_id) VALUES (:name, :email, :password, :pins, :date_of_joining, :faculty_id, :department_id)";
+    $stmt = $this->connection->prepare($query);
+    $stmt->execute($user);
+    $user['id'] = $this->connection->lastInsertId();
+    return $user;
+  }
+
+  public function update_user($id, $user){
+
+  }
+
 }
  ?>
--- api/test.php
+++ api/test.php
@@ -3,7 +3,19 @@ require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 $user_dao = new UserDao();
 
-$user = $user_dao->get_user_by_id(2);
+//$user = $user_dao->get_user_by_id(2);
+
+$user1 = [
+  "name" => "Eldar Jahic",
+  "email" => "eldar.jahic@stu.ibu.edu.ba",
+  "password" => "1234",
+  "pins" => 0,
+  "date_of_joining" => date('Y-m-d H:i:s'),
+  "faculty_id" => 1,
+  "department_id" => 1
+];
+
+$user = $user_dao->add_user($user1);
 
 //$user_dao->get_user_by_email("dino.keco@gmail.com");
 
