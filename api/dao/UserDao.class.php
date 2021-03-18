11378c9386658570836e6c682b1f074487d94a20
VedranSelak <selakvedran@gmail.com>
2021-03-18 15:21:09 +0100

    search users by name added

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -14,5 +14,11 @@ class UserDao extends BaseDao {
     $this->update_by_other_id ($email, $user, "email");
   }
 
+  public function get_users($search, $offset, $limit){
+    return $this->query("SELECT * FROM users
+                         WHERE LOWER(name) LIKE CONCAT('%', :name, '%')
+                         LIMIT ${limit} OFFSET ${offset}",["name" => strtolower($search)]);
+  }
+
 }
  ?>
