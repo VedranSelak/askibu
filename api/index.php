69d84dd12a8330b16c085998d2dcd0379a979ccd
VedranSelak <selakvedran@gmail.com>
2021-03-18 14:56:25 +0100

    added a function for reading query params

--- api/index.php
+++ api/index.php
@@ -2,22 +2,17 @@
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 require_once dirname(__FILE__) . '/dao/UserDao.class.php';
-
+//reading query params from URL
+Flight::map('query',function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return $query_param;
+});
+//register
 Flight::register('userDao', 'UserDao');
-
+//include all routes
 require_once dirname(__FILE__) . "/routes/users.php";
-
-Flight::route('/hello3', function(){
-    echo 'hello world3!';
-});
-
-Flight::route('/hello4', function(){
-    echo 'hello world4!';
-});
-
-Flight::route('/hello5', function(){
-    echo 'hello world5!';
-});
 
 Flight::start();
 
