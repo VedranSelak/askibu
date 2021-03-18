c0dcb12ddb06bc17e770291131475cfb7b3e2b83
VedranSelak <selakvedran@gmail.com>
2021-03-18 14:22:39 +0100

    organized routes for users

--- api/index.php
+++ api/index.php
@@ -1,35 +1,11 @@
 <?php
 
-require dirname(__FILE__) . '/../vendor/autoload.php';
-require dirname(__FILE__) . '/dao/UserDao.class.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/dao/UserDao.class.php';
 
 Flight::register('userDao', 'UserDao');
 
-Flight::route('GET /users', function(){
-    $users = Flight::userDao()->get_all(0,10);
-    Flight::json($users);
-});
-
-Flight::route('GET /users/@id', function($id){
-    $user = Flight::userDao()->get_by_id($id);
-    Flight::json($user);
-});
-
-Flight::route('POST /users', function(){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    $user = Flight::userDao()->add($data);
-    Flight::json($user);
-});
-
-Flight::route('PUT /users/@id', function($id){
-  $request = Flight::request();
-  $data = $request->data->getData();
-  Flight::userDao()->update($id,$data);
-
-  $user = Flight::userDao()->get_by_id($id);
-  Flight::json($user);
-});
+require_once dirname(__FILE__) . "/routes/users.php";
 
 Flight::route('/hello3', function(){
     echo 'hello world3!';
