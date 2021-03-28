a7a298a80b50e6dca97c9404498e0f281f6f3649
VedranSelak <selakvedran@gmail.com>
2021-03-29 00:59:25 +0200

    added some security improvements for tokens

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -24,13 +24,15 @@ class UserService extends BaseService {
   public function reset($user){
     $db_user = $this->dao->get_user_by_token($user["token"]);
     if(!isset($db_user["id"])) throw new Exception("Invalid token",400);
-    $this->dao->update($db_user["id"],["password"=>md5($user["password"])]);
+    if(strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user["token_created_at"]) > 300) throw new Exception("Token expired",400);
+    $this->dao->update($db_user["id"],["password"=>md5($user["password"]), "token"=>NULL]);
   }
 
   public function forgot($user){
     $db_user = $this->dao->get_user_by_email($user["email"]);
     if(!isset($db_user["id"])) throw new Exception("User doesn't exist",400);
-    $db_user = $this->update($db_user["id"],["token" => md5(random_bytes(16))]);
+    if(strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user["token_created_at"]) < 300) throw new Exception("Can't create two tokens within 5 min",400);
+    $db_user = $this->update($db_user["id"],["token" => md5(random_bytes(16)),"token_created_at"=>date(Config::DATE_FORMAT)]);
     $this->smtpClient->send_user_recovery_token($db_user);
   }
 
@@ -69,7 +71,6 @@ class UserService extends BaseService {
         "token" => md5(random_bytes(16))
       ]);
 
-      //TODO send email with token
       $this->dao->commit();
     } catch (\Exception $e) {
       $this->dao->rollBack();
@@ -88,9 +89,7 @@ class UserService extends BaseService {
 
     if (!isset($user['id'])) throw new Exception("Invalid token");
 
-    $this->dao->update($user['id'], ["status" => "ACTIVE"]);
-
-    //TODO send email to user
+    $this->dao->update($user['id'], ["status" => "ACTIVE","token"=>NULL]);
   }
 
 }
