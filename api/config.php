405212cfc16047308d7bd5d2caaaf4c068b27d4b
VedranSelak <selakvedran@gmail.com>
2021-04-07 23:13:01 +0200

    fixes

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
 
   class Config {
-    public static function DATE_FORMAT(){
-      return Config::get_env("DATE_FORMAT","Y-m-d H:i:s");
-    }
+
+    const DATE_FORMAT = "Y-m-d H:i:s";
+
     public static function DB_HOST(){
       return Config::get_env("DB_HOST","localhost");
     }
@@ -17,29 +17,19 @@
       return Config::get_env("DB_SCHEME","forumibudb");
     }
     public static function DB_PORT(){
-      return Config::get_env("DB_PORT",3306);
+      return Config::get_env("DB_PORT","3306");
     }
-    public static function SMTP_HOST(){
-      return Config::get_env("SMTP_HOST","smtp.gmail.com");
-    }
-    public static function SMTP_PORT(){
-      return Config::get_env("SMTP_PORT",587);
-    }
-    public static function SMTP_ENCRYPT(){
-      return Config::get_env("SMTP_ENCRYPT","tls");
-    }
-    public static function SMTP_USER(){
-      return Config::get_env("SMTP_USER","selakvedran@gmail.com");
-    }
-    public static function SMTP_PASSWORD(){
-      return Config::get_env("SMTP_PASSWORD","");
-    }
-    public static function JWT_SECRET(){
-      return Config::get_env("JWT_SECRET","8234854794");
-    }
-    public static function JWT_TOKEN_TIME(){
-      return Config::get_env("JWT_TOKEN_TIME",604800);
-    }
+
+
+    const SMTP_HOST = "smtp.gmail.com";
+    const SMTP_PORT = 587;
+    const SMTP_ENCRYPT = "tls";
+    const SMTP_USER = "selakvedran@gmail.com";
+    const SMTP_PASSWORD = "";
+
+    const JWT_SECRET = "8234854794";
+    const JWT_TOKEN_TIME = 604800;
+
 
     public static function get_env($name, $default){
       return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
