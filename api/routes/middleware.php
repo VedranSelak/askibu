563a654d11d56a01168976872b366a73451872cb
VedranSelak <selakvedran@gmail.com>
2021-05-18 22:06:24 +0200

    added validation and added posting questions

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,9 +1,8 @@
 <?php
 
 Flight::route('/user/*', function(){
-  $token = Flight::header("Authentication");
   try {
-   $user = (array) \Firebase\JWT\JWT::decode($token,Config::JWT_SECRET,["HS256"]);
+   $user = (array) \Firebase\JWT\JWT::decode(Flight::header("Authentication"),Config::JWT_SECRET,["HS256"]);
    if (Flight::request()->method != "GET" && $user["r"] == "USER_READ_ONLY") throw new Exception("Not a real user!",403);
    Flight::set('user', $user);
    return TRUE;
