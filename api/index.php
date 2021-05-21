45af322963c8f696165ac467db397c34e25f2d4e
VedranSelak <selakvedran@gmail.com>
2021-05-22 00:47:16 +0200

    fixed urldecoding and added update questions

--- api/index.php
+++ api/index.php
@@ -18,7 +18,7 @@ Flight::map('query',function($name, $default_value = NULL){
   $request = Flight::request();
   $query_param = @$request->query->getData()[$name];
   $query_param = $query_param ? $query_param : $default_value;
-  return urldecode($query_param);
+  return $query_param;
 });
 
 Flight::map('header', function($name){
