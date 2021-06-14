1c6d7ae3b14452b7abda07a7c7d015f2035937a9
VedranSelak <selakvedran@gmail.com>
2021-06-14 14:33:52 +0200

    refactored some code

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,19 @@
 
-function jsonize_form(selector){
-  var data = $(selector).serializeArray();
-  var form_data = {};
-  for(var i=0; i<data.length;  i++){
-    form_data[data[i].name] = data[i].value; 
+class AskIbuUtils {
+
+  static formToJson(selector){
+    var data = $(selector).serializeArray();
+    var form_data = {};
+    for(var i=0; i<data.length;  i++){
+      form_data[data[i].name] = data[i].value;
+    }
+    return form_data;
   }
-  return form_data;
+
+  static dataToForm(selector, data){
+    for (const attr in data){
+      $(selector+" *[name='"+attr+"']").val(data[attr]);
+    }
+  }
+
 }
