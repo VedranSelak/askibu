1d8967dc3a0f99a4d11c23cba849ee03284e4a3f
VedranSelak <selakvedran@gmail.com>
2021-06-16 16:13:32 +0200

    added initial admin features

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -13,7 +13,16 @@ class AskIbuUtils {
   static dataToForm(selector, data){
     for (const attr in data){
       $(selector+" *[name='"+attr+"']").val(data[attr]);
-    } 
+    }
+  }
+
+  static parseJWT(token){
+    var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
   }
 
 }
